d2b761b6b31c52e80f6fd1ed665e4b7c411b7995
Hyeonnam-J <nami0879@naver.com>
2024-05-23 18:02:13 +0900

    substr id, message

--- src/main.js
+++ src/main.js
@@ -3,17 +3,27 @@
  */
 
 const net = require('net');
+const id = require('./js/id.js');
 
 const PORT = 3000;
 const HOST = '127.0.0.1';
 
 const server = net.createServer( (socket) => {
-    console.log('클라이언트 연결 됨', socket.remoteAddress, socket.remotePort);
-    socket.write('웰컴');
+    // 로그인 기능 생략. 접속한 순서대로 id 발급.
+    const m_id = id.countId++;
+
+    console.log('클라이언트 연결 됨', socket.remoteAddress, ':', socket.remotePort, '/ id: ', m_id);
+    socket.write(m_id.toString());  // 데이터를 보낼 때는 문자열이어야 한다.
 
     socket.on('data', (data) => {
         console.log('받은 데이터: ', data.toString());
+        
+        const index = data.toString().indexOf('/');
+        const client_id = data.toString().substring(0, index);
+        const receivedData = data.toString().substring(index + 1);
 
+        console.log(client_id);
+        console.log(receivedData);
         // todo
         // 유저에게 받은 메시지를 접속한 모든 유저에게 다시 보내는 로직
     });
