7d7317946346f9f210830607ad98fd91e9327c65
Hyeonnam-J <nami0879@naver.com>
2024-05-27 16:23:55 +0900

    nick

--- src/main.js
+++ src/main.js
@@ -13,34 +13,40 @@ const clients = [];
 const server = net.createServer((socket) => {
     // 로그인 기능 생략. 접속한 순서대로 id 발급.
     // clients[] 대신 socket으로 방금 접속한 이에게만 전송.
-    const _id = ID.countId++;
-    const welcomeMessage = new Chat(_id, 'Welcome !', Chat.INFO_TYPE.alarm, true);
-    socket.write(JSON.stringify(welcomeMessage));
-    console.log('클라이언트 연결 됨', socket.remoteAddress, ':', socket.remotePort, '/ id: ', _id);
+    const id = ID.countId++;
+    let nick = '없음';
 
-    // 다른 유저들에게 새 유저 입장 알림 후 클라이언트 목록에 새로 추가.
-    clients.forEach(c => {
-        const entranceAlarm = new Chat(_id, `${_id}님이 대화방에 입장하셨습니다`, Chat.INFO_TYPE.alarm, false);
-        c.write(JSON.stringify(entranceAlarm));
-    });
-    clients.push(socket);
-    console.log('클라이언트 수: ', clients.length);
-    
+    const welcomeMessage = new Chat(id, null, 'Welcome !', Chat.INFO_TYPE.alarm, true);
+    socket.write(JSON.stringify(welcomeMessage));
+    console.log('클라이언트 연결 됨', socket.remoteAddress, ':', socket.remotePort, '/ id: ', id);
+
     socket.on('data', (data) => {
         const json_data = data.toString();
         const obj_data = JSON.parse(json_data);
 
         console.log('클라이언트로부터 받은 메시지: ', json_data);
-        
-        // 유저에게 받은 메시지를 접속한 모든 유저에게 다시 보내는 로직
-        const newMessage = new Chat(obj_data.id, obj_data.message, Chat.INFO_TYPE.message, false);
-        clients.forEach(c => {
-            c.write(JSON.stringify(newMessage));
-        })
+
+        // 다른 방법이..
+        // 다른 유저들에게 새 유저 입장 알림 후 클라이언트 목록에 새로 추가.
+        if(obj_data.issued){
+            nick = obj_data.nick;
+            const entranceAlarm = new Chat(obj_data.id, obj_data.nick, `${obj_data.nick}님이 대화방에 입장하셨습니다`, Chat.INFO_TYPE.alarm, false);
+            clients.forEach(c => {
+                c.write(JSON.stringify(entranceAlarm));    
+            });
+            clients.push(socket);
+            console.log('클라이언트 수: ', clients.length);
+        }else{
+            // 유저에게 받은 메시지를 접속한 모든 유저에게 다시 보내는 로직
+            const newMessage = new Chat(obj_data.id, obj_data.nick, obj_data.message, Chat.INFO_TYPE.message, false);
+            clients.forEach(c => {
+                c.write(JSON.stringify(newMessage));
+            })
+        }
     });
     
     socket.on('end', () => {
-        console.log(`${_id}번 클라이언트 연결 종료`);
+        console.log(`${id}번 클라이언트 연결 종료`);
 
         // 먼저 클라이언트 목록에서 지우고,
         const index = clients.indexOf(socket);
@@ -48,7 +54,7 @@ const server = net.createServer((socket) => {
 
         // 나머지 클라이언트들에게 알림.
         clients.forEach(c => {
-            const exitAlarm = new Chat(_id, `${_id}님이 대화방을 나가셨습니다.`, Chat.INFO_TYPE.alarm, false);
+            const exitAlarm = new Chat(id, nick, `${nick}님이 대화방을 나가셨습니다.`, Chat.INFO_TYPE.alarm, false);
             c.write(JSON.stringify(exitAlarm));
         });
 
