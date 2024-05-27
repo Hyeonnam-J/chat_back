643682ead028090bb93312300820711b6bc880fa
Hyeonnam-J <nami0879@naver.com>
2024-05-27 09:11:54 +0900

    id

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const net = require('net');
-const id = require('./js/id.js');
+const ID = require('./js/id.js');
 const Chat = require('./js/chat.js');
 
 const PORT = 3000;
@@ -13,14 +13,14 @@ const clients = [];
 const server = net.createServer( (socket) => {
     // 로그인 기능 생략. 접속한 순서대로 id 발급.
     // clients[] 대신 socket으로 방금 접속한 이에게만 전송.
-    const m_id = id.countId++;
-    const welcomeMessage = new Chat(m_id.toString(), 'Welcome !', Chat.INFO_TYPE.alarm, true);
+    const _id = ID.countId++;
+    const welcomeMessage = new Chat(_id, 'Welcome !', Chat.INFO_TYPE.alarm, true);
     socket.write(JSON.stringify(welcomeMessage));
-    console.log('클라이언트 연결 됨', socket.remoteAddress, ':', socket.remotePort, '/ id: ', m_id);
+    console.log('클라이언트 연결 됨', socket.remoteAddress, ':', socket.remotePort, '/ id: ', _id);
 
     // 다른 유저들에게 새 유저 입장 알림 후 클라이언트 목록에 새로 추가.
     clients.forEach(c => {
-        const entranceAlarm = new Chat(m_id.toString(), `${m_id}님이 대화방에 입장하셨습니다`, Chat.INFO_TYPE.alarm, false);
+        const entranceAlarm = new Chat(_id, `${_id}님이 대화방에 입장하셨습니다`, Chat.INFO_TYPE.alarm, false);
         c.write(JSON.stringify(entranceAlarm));
     });
     clients.push(socket);
@@ -40,7 +40,7 @@ const server = net.createServer( (socket) => {
     });
     
     socket.on('end', () => {
-        console.log(`${m_id}번 클라이언트 연결 종료`);
+        console.log(`${_id}번 클라이언트 연결 종료`);
 
         // 먼저 클라이언트 목록에서 지우고,
         const index = clients.indexOf(socket);
@@ -50,7 +50,7 @@ const server = net.createServer( (socket) => {
 
         // 나머지 클라이언트들에게 알림.
         clients.forEach(c => {
-            const exitAlarm = new Chat(m_id, `${m_id}님이 대화방을 나가셨습니다.`, Chat.INFO_TYPE.alarm, false);
+            const exitAlarm = new Chat(_id, `${m_id}님이 대화방을 나가셨습니다.`, Chat.INFO_TYPE.alarm, false);
             c.write(JSON.stringify(exitAlarm));
         });
 
